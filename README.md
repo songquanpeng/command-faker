9f4f1677ce97729f7b0203c7f75e19548803a752
JustSong <songquanpeng@foxmail.com>
2022-10-02 19:05:27 +0800

    feat: able to specify output filename now

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Faker
->
+> An executable file generator that used to fake other program.
+
+## Basic Usages
+```shell
+ls | ./faker > tmp && chmod u+x ./tmp && ./tmp
+ls | ./faker tmp && ./tmp
+```
--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <string.h>
 
 #define BUFFER_SIZE 1024
+#define MAX(a, b) (((a)>(b))?(a):(b))
+
 
 char str1[] = "#include <stdio.h>\n"
               "char str[] = \"";
@@ -13,7 +15,7 @@ char str3[] = "\";\n"
               "    printf(\"%s\", str);\n"
               "}";
 
-int main() {
+int main(int argc, char *argv[]) {
     FILE *fp;
     fp = fopen("tmp.c", "w");
     if (fp == NULL) {
@@ -21,10 +23,10 @@ int main() {
         exit(1);
     }
     fwrite(str1, sizeof(char), strlen(str1), fp);
-    char str2[BUFFER_SIZE];
+    char buf[BUFFER_SIZE];
     char escaped_str2[BUFFER_SIZE * 2];
     while (1) {
-        size_t n = read(STDIN_FILENO, str2, BUFFER_SIZE);
+        size_t n = read(STDIN_FILENO, buf, BUFFER_SIZE);
         if (n == -1) {
             printf("Error read from stdin");
             exit(1);
@@ -35,7 +37,7 @@ int main() {
         // n > 0
         int j = 0;
         for (int i = 0; i < n; i++) {
-            switch (str2[i]) {
+            switch (buf[i]) {
                 case '\n':
                     escaped_str2[j++] = '\\';
                     escaped_str2[j++] = 'n';
@@ -53,7 +55,7 @@ int main() {
                     escaped_str2[j++] = '"';
                     break;
                 default:
-                    escaped_str2[j++] = str2[i];
+                    escaped_str2[j++] = buf[i];
                     break;
             }
         }
@@ -61,10 +63,45 @@ int main() {
     }
     fwrite(str3, sizeof(char), strlen(str3), fp);
     fclose(fp);
-    if (system("gcc tmp.c -o tmp") != 0) {
+    char cmd[13 + MAX(3, sizeof(argv[1]))] = "gcc tmp.c -o ";
+    if (argc == 1) {
+        strcat(cmd, "tmp");
+    } else {
+        strcat(cmd, argv[1]);
+    }
+    if (system(cmd) != 0) {
         printf("Error execute gcc");
         exit(1);
     }
-//    remove("tmp.c");
+    remove("tmp.c");
+    if (argc == 1) {
+        fp = fopen("tmp", "rb");
+        if (fp == NULL) {
+            printf("Error read tmp");
+            exit(1);
+        }
+        while (1) {
+            size_t n = fread(buf, 1, BUFFER_SIZE, fp);
+            if (n == -1) {
+                printf("Error read from stdin");
+                exit(1);
+            }
+            if (n == 0) {
+                break;
+            }
+            size_t remain = n;
+            size_t offset = 0;
+            while (remain) {
+                size_t n_wrote = write(STDOUT_FILENO, buf + offset, remain);
+                if (n_wrote == -1) {
+                    printf("Error write to stdout");
+                    exit(1);
+                }
+                remain -= n_wrote;
+                offset += n_wrote;
+            }
+        }
+        remove("tmp");
+    }
     return 0;
 }
